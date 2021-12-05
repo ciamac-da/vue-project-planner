2f3bae00ce060688266f4c3d7e3b200caad6733f
ciamac-da <ciamac.davoudi@gmail.com>
2021-12-05 13:51:08 +0100

    Verified by Marti4n 1.0.3 single project template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,16 +4,21 @@
       <div 
       v-for="project in projects" 
       :key="project.id">
-      <p>{{project.title}}</p>
+      <SingleProject
+      :project="project"
+       />
       </div>
     </div>
   </div>
 </template>
 
+
 <script>
+import SingleProject from '../components/SingleProject.vue'
+
 export default {
   name: 'Home',
-  components: {},
+  components: { SingleProject },
   data () {
     return {
       projects: []
