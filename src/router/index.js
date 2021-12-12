e20e8dd6016e8be96d0a2ee60b589e38e9e6631e
ciamac-da <ciamac.davoudi@gmail.com>
2021-12-12 12:53:55 +0100

    Verified by Marti4n 1.2.0 Edit project route added

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,13 @@ const routes = [
     name: 'AddProject',
     component: () => import('../views/AddProject.vue')
   },
+  {
+    path: '/projects/:id',
+    name: 'EditProject',
+    component: () => import('../views/EditProject.vue'),
+    props: true
+  }
+  
 ]
 
 const router = createRouter({
